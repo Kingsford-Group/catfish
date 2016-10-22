67720d941ed9c6ef63ff24f5072a7253aecbaf6e
Mingfu Shao <shaomingfu@gmail.com>
2016-10-21 20:22:36 -0400

    update

--- src/src/config.h
+++ src/src/config.h
@@ -9,76 +9,27 @@
 using namespace std;
 
 // constants
-#define START_BOUNDARY 1
-#define END_BOUNDARY 2
-#define LEFT_SPLICE 3
-#define RIGHT_SPLICE 4
-#define LEFT_RIGHT_SPLICE 5
-#define MIDDLE_CUT 6
-
 #define TRIVIAL 0
 #define NORMAL 1
 
-#define SLOPE5END 0
-#define SLOPE3END 1
-
-#define SLOPE_MARGIN 0
-#define SLOPE_MIDDLE 1
-
-#define MAX_NUM_CIGAR 7
-
 // user-defined parameters
-extern int min_flank_length;
-extern int32_t min_bundle_gap;
-extern int32_t min_subregion_gap;
-extern int min_num_hits_in_bundle;
-extern int32_t min_splice_boundary_hits;
-extern uint32_t min_mapping_quality;
-extern int32_t average_read_length;
-extern bool identify_slopes;
-extern int32_t average_slope_length;
-extern double min_average_overlap;
-extern double max_indel_ratio;
-extern int32_t min_subregion_length;
-extern int min_max_region_overlap;
-extern double min_region_coverage;
-extern int max_num_bundles;
 extern int max_dp_table_size;
 extern int max_num_subsetsum_solutions;
 extern int max_equations_each_iteration;
-extern double max_equation_error_ratio;
-extern double max_gateway_error_ratio;
-extern int tail_coverage;
-extern int slope_bin_size;
-extern int slope_min_score;
-extern int slope_extend_score;
-extern int slope_min_bin_num;
-extern int slope_flexible_bin_num;
-extern double slope_acceptance_sigma;
-extern bool use_paired_end;
 
-extern double join_min_reliability;
-extern double infer_min_reliability;
-extern double infer_root_reliability;
-extern int pseudo_length_count;
-extern double min_boundary_edge_weight_ratio;
-extern double transcript_min_expression;
-extern bool strand_reverse;
-
-extern int min_hyper_edges_count;
-
-extern string algo;
-extern string input_file;
-extern string ref_file;
-extern string output_file;
-extern bool output_tex_files;
-extern string fixed_gene_name;
-
-extern int min_gtf_transcripts_num;
 extern int simulation_num_vertices;
 extern int simulation_num_edges;
 extern int simulation_max_edge_weight;
+
+extern string algo;
+extern string input_file;
+extern string output_file;
+extern string ref_file;
+extern string fixed_gene_name;
+
 extern bool fast_mode;
+extern bool output_tex_files;
+extern int min_gtf_transcripts_num;
 
 // parse arguments
 bool parse_arguments(int argc, const char ** argv);
--- src/src/main.cc
+++ src/src/main.cc
@@ -4,11 +4,20 @@
 #include <vector>
 #include <iostream>
 #include <ctime>
+#include <cassert>
+#include <sstream>
 
-#include "assembler.h"
 #include "config.h"
+#include "catfish.h"
+#include "gtf.h"
+#include "genome.h"
+#include "nested_graph.h"
+#include "sgraph_compare.h"
 
 using namespace std;
+
+int assemble_gtf(const string &file);
+int assemble_sgr(const string &file);
 
 int main(int argc, const char **argv)
 {
@@ -16,9 +25,61 @@ int main(int argc, const char **argv)
 	parse_arguments(argc, argv);
 	print_parameters();
 
+	if(input_file == "" && output_file != "")
+	{
+		splice_graph sg;
+		sg.simulate(simulation_num_vertices, simulation_num_edges, simulation_max_edge_weight);
+		sg.write(output_file);
+		return 0;
+	}
 
-	assembler asmbl;
-	asmbl.process();
+	if(input_file != "")
+	{
+		string s = input_file.substr(input_file.size() - 3, 3);
+		if(s == "gtf") assemble_gtf(input_file);
+		if(s == "sgr") assemble_sgr(input_file);
+	}
+	return 0;
+}
 
-    return 0;
+int assemble_gtf(const string &file)
+{
+	genome g(file);
+
+	ofstream fout;
+	if(output_file != "") fout.open(output_file);
+
+	for(int i = 0; i < g.genes.size(); i++)
+	{
+		gtf gg(g.genes[i]);
+
+		if(gg.transcripts.size() < min_gtf_transcripts_num) continue;
+
+		string name = gg.get_gene_id();
+
+		if(fixed_gene_name != "" && name != fixed_gene_name) continue;
+
+		splice_graph gr;
+		gg.build_splice_graph(gr);
+
+		catfish sc(name, gr);
+		sc.assemble();
+
+		if(output_file == "") continue;
+		gg.output_gtf(fout, sc.paths, algo);
+	}
+
+	if(output_file != "") fout.close();
+	return 0;
+}
+
+int assemble_sgr(const string &file)
+{
+	splice_graph sg;
+	sg.build(file);
+
+	catfish sc("shao", sg);
+	sc.assemble();
+
+	return 0;
 }
